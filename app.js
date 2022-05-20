e0020d1db2fb2041abebe8cfe4640c9192f63a0a
Luana Roza <91756435+Luh095@users.noreply.github.com>
2022-05-20 14:10:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import './App.css';
+
+import Header from './components/header';
+import Footer from './components/footer';
+    
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
